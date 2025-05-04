65596658e2ab55eca0675c34da1a65d0a21e96c3
PavelBzrkv <pavel95959@gmail.com>
2025-05-04 20:37:29 +0500

    Commit task1

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -5,5 +5,15 @@ public class Main {
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
     public static void main(String[] args) {
         //todo код писать тут
+        String letters = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+        for (int i =0; i < letters.length(); i++) {
+            System.out.println(letters.charAt(i) + "-" + (int)letters.charAt(i));
+            System.out.println(letters.toUpperCase().charAt(i) + "-" + (int)letters.toUpperCase().charAt(i));
+            System.out.println("======");
+        }
+
+
+
     }
 }
